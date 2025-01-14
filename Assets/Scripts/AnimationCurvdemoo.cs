9da78bdffc711cf0ef6bf78f62e613a222e9bf3c
delmastv <delmastv@sheridancollege.ca>
2025-01-14 17:41:23 -0500

    Animation curve in class

---
+++ Assets/Scripts/AnimationCurvdemoo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class AnimationCurvdemoo : MonoBehaviour
+{
+    
+public AnimationCurve curve;
+
+[Range(0, 1)]
+public float t;
+{
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1)
+        { t = 0}
+
+
+        transform.localScale = Vector3.one * curve.Evaluate(t);
+    }
+}
--- Assets/Scripts/Weektwopartone.cs
+++ Assets/Scripts/Weektwopartone.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public float rotspeed =4;
-public class Weektwopartone : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//public float rotspeed =4;
+//public class Weektwopartone : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
        
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 = Camera.main.ScreenToWorldPoint();
-        Vector2 direction = 
-        transform.forward =direction;
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Vector2 = Camera.main.ScreenToWorldPoint();
+//        Vector2 direction = 
+//        transform.forward =direction;
+//    }
+//}
