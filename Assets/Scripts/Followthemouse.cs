fe21820973d0c9aaa2f525bda9cee5078cfb5c3d
delmastv <delmastv@sheridancollege.ca>
2025-01-14 17:05:18 -0500

    commiting new script and unfinished work on sourcetree

--- Assets/Scripts/Followthemouse.cs
+++ Assets/Scripts/Followthemouse.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Followthemouse : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class Followthemouse : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Camera.main.ScreenToWorldPoint(Input. mouesePosition);
-        transform.position = mousePos;
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        transform.position = Camera.main.ScreenToWorldPoint(Input. mouesePosition);
+//        transform.position = mousePos;
+//    }
+//}
---
+++ Assets/Scripts/Weektwopartone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public float rotspeed =4;
+public class Weektwopartone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 = Camera.main.ScreenToWorldPoint();
+        Vector2 direction = 
+        transform.forward =direction;
+    }
+}
