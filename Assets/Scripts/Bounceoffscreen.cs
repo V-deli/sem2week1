b8475dadc4a396e02dc1a16ee4ec926244ae32b5
delmastv <delmastv@sheridancollege.ca>
2025-01-14 20:37:58 -0500

    fixed bouncing square

--- Assets/Scripts/Bounceoffscreen.cs
+++ Assets/Scripts/Bounceoffscreen.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Bounceoffscreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 0.1f;
+
+
     void Start()
     {
         
@@ -13,6 +15,15 @@ public class Bounceoffscreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            speed = speed * -1;
+        }
+        transform.position = pos;
     }
 }
