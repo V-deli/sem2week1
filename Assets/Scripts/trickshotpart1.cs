ce56cbf0bc3acb71cf56edeca0320780ed9ffbfa
delmastv <delmastv@sheridancollege.ca>
2025-01-21 17:52:23 -0500

    trick shot part 2 coding gym

--- Assets/Scripts/trickshotpart1.cs
+++ Assets/Scripts/trickshotpart1.cs
@@ -9,18 +9,19 @@ public class trickshotpart1 : MonoBehaviour
 {
     public float speed = 3f; //made speed variable to alter the time of speed when moving
     public AnimationCurve curve;
-    [Range(0, 1)] public float t;
+    [Range(0, 1)] public float t; //curve
     
 
     void Start()
 {
-       
+        t = 0; 
 }
 
     void Update()
     {
         Vector3 cirPos = transform.position;
         cirPos.x += speed; //transforming movemement of x speed
+        cirPos.z = 0;
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(cirPos);
 
@@ -38,10 +39,12 @@ public class trickshotpart1 : MonoBehaviour
             {
                 t = 0;
             }
-
-
-            cirPos.y = curve.Evaluate(t);
+         cirPos.y = curve.Evaluate(t);
+            Vector3 rot = transform.eulerAngles;
+            rot.z = curve.Evaluate(t);
+            transform.eulerAngles = rot;
         }
+
         transform.position = cirPos;
         }
     }
