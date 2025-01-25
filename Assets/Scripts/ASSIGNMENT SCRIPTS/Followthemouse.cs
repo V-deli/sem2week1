190b05f92dc7b2ae294544a0aa387fc0e2754fbe
delmastv <delmastv@sheridancollege.ca>
2025-01-25 15:32:59 -0500

    I made it so the bee cant travel to the bottom of the screen

--- Assets/Scripts/ASSIGNMENT SCRIPTS/Followthemouse.cs
+++ Assets/Scripts/ASSIGNMENT SCRIPTS/Followthemouse.cs
@@ -14,6 +14,13 @@ public class Followthemouse : MonoBehaviour
     void Update()
     {
         Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float screenlimitbelow = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, 0)).y;
+
+        if (mouse.y < screenlimitbelow)
+        {
+            mouse.y = screenlimitbelow;
+        }
+
         transform.position = mouse;
     }
 }
