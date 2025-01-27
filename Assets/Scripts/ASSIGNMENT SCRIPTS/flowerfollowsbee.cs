935866a8c6246fae893ba6604ed8379441541b24
delmastv <delmastv@sheridancollege.ca>
2025-01-27 10:51:04 -0500

    reviewing work

--- Assets/Scripts/ASSIGNMENT SCRIPTS/flowerfollowsbee.cs
+++ Assets/Scripts/ASSIGNMENT SCRIPTS/flowerfollowsbee.cs
@@ -14,7 +14,7 @@ public class flowerfollowsbee : MonoBehaviour
     void Update()
     {
        Vector3 beeposition = Camera.main.ScreenToWorldPoint( Input.mousePosition ); // the mouse position converts from screen position to world.
-        beeposition.z = 0; // setting z pos to 0 since its 2d not needed
+        beeposition.z = 0; // setting z pos to 0 
         Vector2 facing = beeposition - transform.position; //calculates the direction vector from flower to bee/mouse
 
         transform.up = facing; //moves the flower to face the direction of bee, by setting the vector to that position
