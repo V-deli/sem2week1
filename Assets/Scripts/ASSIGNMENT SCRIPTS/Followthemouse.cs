f89d55d1f9bd90da3f206546d63bef17da9b5bec
delmastv <delmastv@sheridancollege.ca>
2025-01-27 11:13:17 -0500

    added some comments It seems i forgot to fill

--- Assets/Scripts/ASSIGNMENT SCRIPTS/Followthemouse.cs
+++ Assets/Scripts/ASSIGNMENT SCRIPTS/Followthemouse.cs
@@ -13,15 +13,15 @@ public class Followthemouse : MonoBehaviour
 
     void Update()
     {
-        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); //the bee is being altered in the world screen, the mousePosition is being affected/used
         float screenlimitbelow = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, 0)).y;
 
-        if (mouse.y < screenlimitbelow)
+        if (mouse.y < screenlimitbelow) //if statment saying that if the y mouse position is less than the top half of the screen then it can preform the transformation of following the mouse position
         {
-            mouse.y = screenlimitbelow;
+            mouse.y = screenlimitbelow; //screen height/2... mouse y is in the top half of the height of the screen
         }
 
-        transform.position = mouse;
+        transform.position = mouse; //this line of code activates the bee to follow the position of the mouse
     }
 }
 //Using for the bee in Assignment One
--- Assets/Scripts/ASSIGNMENT SCRIPTS/beebouncingscreen.cs
+++ Assets/Scripts/ASSIGNMENT SCRIPTS/beebouncingscreen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class beebouncingscreen : MonoBehaviour
 {
-    public float bounceSpeed = 5f;
+    public float bounceSpeed = 5f; //variable for the speed the beetravels across the screen horizontally, can be edited in the inspector
 
 
     void Start()
@@ -12,18 +12,18 @@ public class beebouncingscreen : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += bounceSpeed;
+        Vector3 pos = transform.position; //new vector to transform the position of an object
+        pos.x += bounceSpeed; //altering the x position
 
-        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos); //the bee position is being edited in the screen point
 
-        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        if (screenPos.x < 0 || screenPos.x > Screen.width) //2 restraints of the screen edges for the bee, if the 
         {
-            bounceSpeed = bounceSpeed * -1;
+            bounceSpeed = bounceSpeed * -1; // if the 2 conditions are met then this line of code runs saying the bee moves at this speed and once it hits the edge it goes the opposite way due to it being multiplyed by a -1
         }
-        transform.position = pos;
+        transform.position = pos; //this reiterates that the position of the object is being altered
     }
 }
